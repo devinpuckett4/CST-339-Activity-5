f2b181540b7f76b8fc6728d0c8ae187311e522b7
Devin Puckett <devinpuckett4@gmail.com>
2025-09-25 19:22:32 -0400

    Track topic4-2 as regular files

---
+++ topic4-2/src/main/java/com/gcu/topic2/Topic51Application.java
@@ -0,0 +1,11 @@
+package com.gcu.topic2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Topic51Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Topic51Application.class, args);
+    }
+}
